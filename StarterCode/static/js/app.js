7a5378fdaabaa7e94be7bd559ccd82506cc07736
Gerald Watts <geraldwattsthethird@gmail.com>
2021-04-21 21:31:17 -0500

    Wed Update

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -27,7 +27,7 @@ function init() {
 
     }));
 }
-
+//create function to reset data
 function resetData() {
 
     demographicsTable.html("");
@@ -37,12 +37,15 @@ function resetData() {
 
 };
 
+//create function to plot charts
 function plotCharts(id) {
 
+        //read in json data
         d3.json("data/samples.json").then((data => {
             var singleMetadata = data.metadata.filter(participant => participant.id == id)[0];
             var wordFrequency = singleMetadata.wordFrequency;
 
+        //iterate through key and value data
         Object.defineProperties(singleMetadata).forEach(([key, value]) => {
 
             var newTable = demographicsTable.append("ul");
@@ -53,17 +56,91 @@ function plotCharts(id) {
 
             tableItem.text(`${key}: ${value}`);
         
-        )};
+        });
 
         var singleSample = data.samples.filter(sample => sample.id == id)[0];
         var otuIds = [];
         var otuLabels = [];
         var sampleValues = [];
+        
+        //iterate through key and value data
+        Object.defineProperties(singleSample).forEach(([key, value]) => {
 
-        Object.defineProperties(singleSample).forEach(([key, value]){
+            switch (key) {
+                case "otu_ids":
+                    otuIds.push(value);
+                    break;
+                case "sample_values":
+                    sampleValues.push(value);
+                    break;
+                case "otu_labels":
+                    otuLabels.push(value);
+                    break;
+                default:
+                    break;
+            };
+        });
 
-            
-        })
-            
-        ))
-}
+        var maxOtuIds = otuIds[0].slice(0, 10).reverse();
+        var maxOtuLabels = otuLabels[0].slice(0, 10).reverse();
+        var maxSampleValues = sampleValues[0].slice(0, 10).reverse();
+
+        var topOtuIdsFormatted = topOtuIds.map(otuID => "OTU " + otuID);
+
+        //plot bar chart
+
+        //create trace for bar chart
+        var traceBarchart = {
+            x: topSampleValues,
+            y: topOtuIdsFormatted,
+            text: topOtuLabels,
+            type: 'bar',
+            orientation: 'h',
+            marker: {
+                color: 'rgb(29,145,192)'
+            }
+        };
+
+        //create data array
+        var dataArraybar = [traceBarchart];
+
+        //create plot layout
+        var layoutBarchart = {
+            height: 500,
+            width: 600,
+            font: {
+                family: 'Quicksand'
+            },
+            hoverlabel: {
+                font: {
+                    family: 'Quicksand'
+                }
+            },
+            title: {
+                text: `<b>Top OTUs for Test Subject ${id}</b>`,
+                font: {
+                    size: 18,
+                    color: 'rgb(34,94,168)'
+                }
+            },
+            xaxis: {
+                title: "<b>Sample values<b>",
+                color: 'rgb(34,94,168)'
+            },
+            yaxis: {
+                tickfont: { size: 14 }
+            }
+        }
+
+        //create bar chart
+        Plotly.newPlot("bar", dataArraybar, layoutBarchart);
+
+        //Bubble Chart
+
+
+
+
+
+    });
+
+});
